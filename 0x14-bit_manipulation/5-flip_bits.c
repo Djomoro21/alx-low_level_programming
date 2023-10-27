a3ac7380cb2ca5dae21d9a9b06b1d24827af9769
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-27 02:19:55 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,47 +1,21 @@
 #include "main.h"
 
 /**
- * flip_bits - a functiogfdfdn that returns the number of bits you
- *             would need to flip gdgfddgdfto get from one number to
- *             another
+ * flip_bits - rgvghvhgvkhj kjbjkhbld
+ * @n: nnlkjklj lhblke.
+ * @m: njhbj hbjbjb.
  *
- * @n: decimal ngfdfgdumber
- * @m: the other gfdgfddecimal number
- *
- * Return: number of bits to flip to get @m from @n
-*/
+ * Return: nubjhbjhb bbhbjbkts.
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int diff_bits = 0;
-	unsigned long int one = 1;
-	unsigned long int diff;
+	unsigned int numbits;
 
-	/*xor n and gddgdb*/
-	m *= one;
-	n *= one;
-	diff = n ^ m;
+	for (numbits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			numbits++;
+	}
 
-	printf("\n(n) %ld and (m) %ld in binary is ", n, m);
-	show_bin(n);
-	printf(" ");
-	show_bin(m);
-	printf(" diff is %ld and in binary is ", diff);
-	show_bin(diff);
-	printf("\n");
-
-	/*keep shifting diff tfhfhfo right*/
-	do {
-		printf("diff %ld before right shift ", diff);
-		show_bin(diff);
-		printf("\n");
-
-		diff_bits += (diff & 1);
-		diff >>= 1;
-
-		printf("diff %ld after right shift ", diff);
-		show_bin(diff);
-		printf(" and diff_bit %d before leaving loop\n\n", diff_bits);
-	} while (diff > 0);
-
-	return (diff_bits);
+	return (numbits);
 }
