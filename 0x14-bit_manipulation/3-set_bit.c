a646c0067af0bc53019ca2a82c087ede8b7175c9
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-27 02:14:54 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,44 +1,21 @@
 #include "main.h"
 
-#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
-#define CHAR_BITS 8 /*size of char*/
-
-
 /**
- * set_bit - sets the valuhbkjnjknbit to 1 at a given
- *           index.
+ * set_bit - setbhjbjhbj;  1.
+ * @n: po b,b,nb ;,nb;b;nt.
+ * @index: ijnkjnkjnkjnkit.
  *
- * @n: num
- * @index: index to set the value ofnnbjhbjb bit to 1
- *
- * Return: 1 or -1
-*/
+ * Return: 1 ifknkjnkjnkjn:kn't.
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int zero = 0;
-	unsigned long int one = 1;
-	unsigned long int mask = 1;
-	unsigned long int n_bfr = *n;
+	unsigned int myvar;
 
-	index = index + zero;
-	index = index * one;
-
-
-	/*if index is greater than size of n in binary coded decimal*/
-	if (index > INT_BITS)
+	if (index > 63)
 		return (-1);
 
-	mask <<= index; /*create mask based on index position*/
-
-	*n = (*n | mask);
-
-	printf("\n%ld in binary is ", n_bfr);
-	my_show_bin(n_bfr);
-	printf(" while mask right shifted to index %d is ", index);
-	my_show_bin(mask);
-	printf(" our new value n in binary is ");
-	my_show_bin(*n);
-	printf("\n\n");
+	myvar = 1 << index;
+	*n = (*n | myvar);
 
 	return (1);
 }
