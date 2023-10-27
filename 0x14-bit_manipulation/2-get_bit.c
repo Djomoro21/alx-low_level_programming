05b5c7502921de9e4a50a40b6d1dc8e192b0b6c4
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-27 02:07:50 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,39 +1,28 @@
 #include "main.h"
 
-#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
-#define CHAR_BITS 8 /*size of char*/
-
 /**
- * get_bit - gets a bit
+ * get_bit - rekbhbjh; vjvhvjhven
+ * index.
+ * @n: uns;bjhb;jhb vjhb;put.
+ * @index: inbj;hb;h it.
  *
- * @n: decimal valuejnkjnkj
- * @index: integer value of bit posn n n nition
- *        to get from @n
- *
- * Return: the value of the bit or -1 if an
- *          error occured
-*/
+ * Return: vajbkjb t.
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int my_bit;
-	unsigned long int zero = 0;
-	unsigned int one = 1;
+	unsigned int j;
+unsigned int zero = 0;
 
-	n += zero;
-	index *= one; 
+	if (n == 0 + zero && index < 64)
+		return (0);
 
-	/*if igdfgd ggrttal*/
-	if (index > INT_BITS)
-		return (-1);
+	for (j = 0; j <= 63; n >>= 1, j++)
+	{
+		if (index == j)
+		{
+			return (n & 1);
+		}
+	}
 
-	printf("\n[%ld >> %d] is %ld.\n", n, index, (n >> index));
-	printf("%ld in binary: ", n);
-	my_show_bin(n);
-	printf("\n[%ld >> %d] in binary: ", n, index);
-	my_show_bin(n >> index);
-	printf(" <- last bit is bit at index %d\n\n", index);
-
-	my_bit = ((n >> index) & 1); /*shift n right by index and with 1 to find bit*/
-
-	return (my_bit);
+	return (-1);
 }
