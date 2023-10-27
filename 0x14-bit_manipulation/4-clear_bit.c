ee4d63d7d923c72cffb9a30c4816330b5c37fd4b
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-27 02:16:53 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0.
- * at a given index.
- * @n: pointer of an unsigned long int.
- * @index: index of the bit.
+ * clear_bit - sevhgvgvgh gvgvhgvh0.
+ * @n: poinbjhbjh hbjhbjint.
+ * @index: ijbhjhbjh hbjhbjjb.
  *
- * Return: 1 if it worked, -1 if it didn't.
+ * Return: hbhjbj jhbjhbjkbj hbbhj't.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
