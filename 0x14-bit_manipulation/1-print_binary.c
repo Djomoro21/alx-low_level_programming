61f8b377a88d5f9353cd702dafe80546c863900f
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-27 01:57:29 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,22 @@
 #include "main.h"
 
 /**
-* is_greater_than_zero - checks if a number is greater than zero.
-* @n: unsigned long int.
-*
-* Return: 1 if n is greater than zero, 0 otherwise.
-*/
-int is_greater_than_zero(unsigned long int n)
-{
-return (n >> 1);
-}
-
-/**
-* print_binary - prints the binary representation of a number.
-* @n: unsigned long int.
-*
-* Return: no return.
-*/
+ * print_binary - phjvjhvkjvk hjvvjhgvh gvhtion
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
+ */
 void print_binary(unsigned long int n)
 {
-if (is_greater_than_zero(n))
-{
-print_binary(n >> 1);
-_putchar((n & 1) + '0');
-}
-else
-{
-_putchar('0');
-}
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
