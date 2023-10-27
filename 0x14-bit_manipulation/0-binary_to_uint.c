3b064f2526e273bb4cc285591a43a439460d30f2
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-27 01:08:17 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,78 @@
 #include "main.h"
 
 /**
- * binary_to_uint - This jhhbkjbkj jhbhb to fghjbkjk bkghkjnary number
+ * _atoi - converts chars to integer
+ *
+ * @c: char to convert
+ *
+ * Return: converted integer
+*/
+unsigned int _atoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+
+/**
+ * _strlen - gets the length of a string
+ *
+ * @str: string input
+ *
+ * Return: string length
+*/
+unsigned int _strlen(const char *str)
+{
+	unsigned int index = 0;
+
+	while (str[index] != '\0')
+		index++;
+	return (index);
+}
+
+/**
+ * binary_to_uint - a function that converts a binary number
  *                  to an unsigned int
  *
- * @b: string khbjhbk;jnkj
+ * @b: string that contains 0 and 1 characters
  *
- * Return:  convertygbkjllk hbkj 0
+ * Return: the number converted or 0
+ *         if @b contains a character
+ *         that is not 0 or 1 or when
+ *         @b is null
 */
 unsigned int binary_to_uint(const char *b)
 {
-	int indx;
-	unsigned int resultat = 0;
-	unsigned int b2 = 1;
-	unsigned int n = 0;
-	unsigned int zero = 0;
-	unsigned int one = 1;
+	int index;
+	unsigned int result = 0, base2 = 1,  num = 0;
 
+	/*if b is NULL return 0*/
 	if (b == NULL)
 		return (0);
 
+	#ifdef DEBUG /*print debug statement*/
+	printf("String is %s, and length is %u.\n", b, _strlen(b));
+	#endif
 
-	/*iterabbte through strkjkhghing*/
-	for (indx = _mystrlength(b) - 1; indx >= 0; indx--)
+	/*iterate through string*/
+	for (index = _strlen(b) - 1; index >= 0; index--)
 	{
-		n = _mycharstoint(b[indx]); /*conbn;n,;nnb,bber*/
+		num = _atoi(b[index]); /*convert char to number*/
 
+		#ifdef DEBUG /*print debug statements*/
+		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
+		#endif
 
-		/*if number is not 0 or 1 returnhjkjn 0*/
-		if (n != 0 && n != 1)
+		/*if number is not 0 or 1 return 0*/
+		if (num != 0 && num != 1)
 			return (0);
 
-		resultat += n * b2; /*enablhbkjnkj hbkjkjlction*/
-		b2 *= 2;
-		b2 += zero;
-		b2 *= one;
+		result += num * base2; /*enable debug to see it in action*/
+		base2 *= 2;
 
+		#ifdef DEBUG /*print debug statements*/
+		printf("Result is %u.\n", result);
+		#endif
 	}
 
-	return (resultat);
+	return (result);
 
 }
