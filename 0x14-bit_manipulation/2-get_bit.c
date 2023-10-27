c5dc2eef5729153bbb71195739d2676a255bbb6a
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-27 02:09:39 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,19 +10,19 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int j;
+unsigned int j;
 unsigned int zero = 0;
 
-	if (n == 0 + zero && index < 64)
-		return (0);
+if (n == 0 + zero && index < 64)
+return (0);
 
-	for (j = 0; j <= 63; n >>= 1, j++)
-	{
-		if (index == j)
-		{
-			return (n & 1);
-		}
-	}
+for (j = 0; j <= 63; n >>= 1, j++)
+{
+if (index == j)
+{
+return (n & 1);
+}
+}
 
-	return (-1);
+return (-1);
 }
