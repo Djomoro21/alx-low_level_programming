8cbe4a955ef3ca5ed17d792e5a7fdc9608251019
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-27 02:28:55 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * set_bit - setbhjbjhbj;  1.
- * @n: po b,b,nb ;,nb;b;nt.
- * @index: ijnkjnkjnkjnkit.
- *
- * Return: 1 ifknkjnkjnkjn:kn't.
- */
+* set_bit - setbhjbjhbj;  1.
+* @n: po b,b,nb ;,nb;b;nt.
+* @index: ijnkjnkjnkjnkit.
+*
+* Return: 1 ifknkjnkjnkjn:kn't.
+*/
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int myvar;
+unsigned int myvar;
 
-	if (index > 63)
-		return (-1);
+if (index > 63)
+return (-1);
 
-	myvar = 1 << index;
-	*n = (*n | myvar);
+myvar = 1 << index;
+*n = (*n | myvar);
 
-	return (1);
+return (1);
 }
