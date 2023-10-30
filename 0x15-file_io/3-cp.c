22f8714473dd546d6ff0608fef605624bc381aa2
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-30 18:48:43 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
 /**
  * main - check the code for Holberton School students.
  * @argc: number of arguments.
@@ -37,12 +56,11 @@ int main(int argc, char *argv[])
 	{
 		nchars = read(file_in, buf, 1024);
 		if (nchars == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
+			error_file(-1, 0, argv);
 		nwr = write(file_out, buf, nchars);
 		if (nwr == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			error_file(0, -1, argv);
 	}
 
 	err_close = close(file_in);
