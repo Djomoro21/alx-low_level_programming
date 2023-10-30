a0cf3adaaee4b4f0463b58cb1aca8b8fa7f808d0
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-30 16:03:57 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,8 +13,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
-	ssize_t read_check, wcount;
-	char *buffer;
+	ssize_t nrd, nwr;
+	char *buff;
 
 	if (filename == NULL) /*check if file is present*/
 		return (0);
@@ -24,25 +24,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (file == -1)
 		return (0);
 
-	/*get the size of buffer from number of letters*/
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	/*get the size of buff from number of letters*/
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 	{
-		free(buffer);
+		free(buff);
 		return (0);
 	}
 
-	read_check = read(file, buffer, letters); /*read file*/
-	if (read_check == -1) /*check if read failed*/
+	nrd = read(file, buff, letters); /*read file*/
+	if (nrd == -1) /*check if read failed*/
 		return (0);
 
-	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
-	if (wcount == -1 || read_check != wcount) /*check if write failed*/
+	nwr = write(STDOUT_FILENO, buff, nrd); /*write to POSIX*/
+	if (nwr == -1 || nrd != nwr) /*check if write failed*/
 		return (0);
 
-	free(buffer);
+	free(buff);
 
 	close(file); /*close file*/
 
-	return (wcount);
+	return (nwr);
 }
