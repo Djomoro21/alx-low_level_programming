26d115d9195768c16044762880d423018df48032
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-30 18:22:17 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,25 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * error_file - checks if files can be opened.
- * @file_in: file_in.
- * @file_out: file_out.
- * @argv: arguments vector.
- * Return: no return.
- */
-void error_file(int file_in, int file_out, char *argv[])
-{
-	if (file_in == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_out == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
 
 /**
  * main - check the code for Holberton School students.
@@ -35,13 +15,22 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_in file_out");
 		exit(97);
 	}
 
 	file_in = open(argv[1], O_RDONLY);
+	if (file_in == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	file_out = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_in, file_out, argv);
+	if (file_out == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 
 	nchars = 1024;
 	while (nchars == 1024)
