15c2f37a293f44d7f8851edc80b8367779384f76
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-30 19:09:22 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,7 +29,7 @@ void my_exit(int file_from, int file_to, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
+	int file_from, file_to, clos;
 	ssize_t num, stats;
 	char my_buff[1024];
 
@@ -54,15 +54,15 @@ int main(int argc, char *argv[])
 			my_exit(0, -1, argv);
 	}
 
-	err_close = close(file_from);
-	if (err_close == -1)
+	clos = close(file_from);
+	if (clos == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(file_to);
-	if (err_close == -1)
+	clos = close(file_to);
+	if (clos == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
