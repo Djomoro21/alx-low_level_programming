c39d7c9bbc81e701f4ea8ec3e3855aa35fc0aee9
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-30 16:30:30 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, write_status, words = 0;
 
-	if (filename == NULL) /*check if filename is present*/
+	if (!filename) /*check if filename is present*/
 		return (-1);
 
 	/*open file by creating it and if it exists write but truncate to 0*/
@@ -29,7 +29,8 @@ int create_file(const char *filename, char *text_content)
 		write_status = write(file, text_content, words);
 		if (write_status == -1) /*check if write was a success*/
 			return (-1);
-	}
+	}else
+		text_content = "";
 
 	close(file); /*close file*/
 	return (1);
