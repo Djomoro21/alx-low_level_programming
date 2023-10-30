22934fcf8868c4587e625c5b6dcb1fcbbfa9b22a
Sylla <138716442+Djomoro21@users.noreply.github.com>
2023-10-30 17:00:09 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,38 @@
 #include "main.h"
 
 /**
- * append_text_to_file - a funcion that appends text at the end
- *                      of a file
+ * append_text_to_file - a fvghvhgv gvhgch tjfhfend
+ *                      oghhg gvhg ghge
  *
- * @filename: file to add data to
- * @text_content: text content to add to file
+ * @filename: fhjvjhvjh jvjhgvhgvh
+ * @text_content: thbjhbj hjbjhvhjv gvjgvjvkje
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 hvhvk gkvjvjh gvjghvhjvjhure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, app_status, words = 0;
+	int my_fd;
+	int aps, wcount = 0;
 
-	if (filename == NULL) /*check if file is present*/
+	if (filename == NULL) /*cvjhvkjvh kgvghch jgchgvhnt*/
 		return (-1);
 
-	/*open file, with append option with write rights*/
-	file = open(filename, O_APPEND | O_WRONLY);
-	if (file == -1) /*check if file is present*/
+	/*opgvgvg hgvjuhgkh gvhgvhjgvhghts*/
+	my_fd = open(filename, O_APPEND | O_WRONLY);
+	if (my_fd == -1) /*chebn ,b ,;n,bvjvkjvj gvhjjbent*/
 		return (-1);
 
-	if (text_content) /*append content to file if its not NULL*/
+	if (text_content) /*appvvjhvj jvjvhgv ghcghhULL*/
 	{
-		while (text_content[words] != '\0') /*find number of words*/
-			words++;
+		while (text_content[wcount] != '\0') /*fvhv,;b,b jvgfhcjhgvjnt*/
+			wcount++;
 
-		/*append to file*/
-		app_status = write(file, text_content, words);
-		if (app_status == -1) /*check if append was a success*/
+		/*apjkghkhv fytfye*/
+		aps = write(my_fd, text_content, wcount);
+		if (aps == -1) /*chevhvbvn, gvghvhss*/
 			return (-1);
 	}
 
-	close(file); /*close file*/
+	close(my_fd); /*clobvhjb jbkjhbe*/
 	return (1);
 }
